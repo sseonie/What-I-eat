8b2f845e3e79b97366bbd0559e46b618a71380c0
sseonie <sunnymin0318@gmail.com>
2026-09-08 23:36:28 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'food-diary-v3';
+const CACHE = 'food-diary-v10';
 const ASSETS = ['./', './index.html', './auth-sync.js', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', event => {
