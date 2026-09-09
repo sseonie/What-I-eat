9f53a3c4de949707a2a5120e379d7094f5d1a96e
sseonie <sunnymin0318@gmail.com>
2026-09-09 11:55:48 +0800

    Refresh app cache automatically
    Updated cache version and added fetch handling for navigation requests.

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'food-diary-v16';
+const CACHE = 'food-diary-v17';
 const ASSETS = ['./', './index.html', './auth-sync.js', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', event => {
@@ -12,5 +12,13 @@ self.addEventListener('activate', event => {
 });
 
 self.addEventListener('fetch', event => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith(fetch(event.request).then(response => {
+      const copy = response.clone();
+      caches.open(CACHE).then(cache => cache.put('./index.html', copy));
+      return response;
+    }).catch(() => caches.match('./index.html')));
+    return;
+  }
   event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request)));
 });
